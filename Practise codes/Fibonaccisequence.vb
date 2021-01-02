0fe33305cb1a90e6357304ef01e59b027fbd1797
turabusmani <74224711+turabusmani@users.noreply.github.com>
2021-01-02 17:11:30 +0500

    Add files via upload

---
+++ Practise codes/Fibonaccisequence.vb
@@ -0,0 +1,24 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim num1 As Integer = 0
+        Dim num2 As Integer = 1
+        Dim numnext As Integer = 0
+
+        Console.WriteLine(num1)
+        Console.WriteLine(num2)
+
+        For count = 1 To 10
+
+            numnext = num1 + num2
+            num1 = num2
+            num2 = numnext
+
+            Console.WriteLine(numnext)
+        Next
+
+        Console.ReadKey()
+    End Sub
+
+End Module
